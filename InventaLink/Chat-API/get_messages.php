2e2415c570540aa36494aafa29d4211c098253d7
Amelia Vega <133181467+AmeliaVegaa@users.noreply.github.com>
2024-12-18 20:00:49 +0700

    Delete InventaLink/Chat-API directory

--- InventaLink/Chat-API/get_messages.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-if (!isset($_SESSION['messages'])) {
-    $_SESSION['messages'] = [];
-}
-echo json_encode($_SESSION['messages']);
