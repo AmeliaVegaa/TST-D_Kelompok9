1753c680639341ac9588a5c0453025e882c93d0b
Amelia Vega <133181467+AmeliaVegaa@users.noreply.github.com>
2024-12-18 20:16:27 +0700

    Create index.php

---
+++ Utama/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integrasi Sistem</title>
+</head>
+
+<body>
+    <h1>Integrasi Sistem</h1>
+
+    <h2>Sistem Inventory</h2>
+    <a href="sistem_inventory/index.php">Tambah dan Get Item</a>
+
+    <h2>Sistem Penjualan</h2>
+    <a href="sistem_penjualan/index.php">Checkout dan Get Transaksi</a>
+
+    <h2>Sistem Tracking</h2>
+    <a href="sistem_tracking/index.php">Track Transaksi</a>
+</body>
+
+</html>
